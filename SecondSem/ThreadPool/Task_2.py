0c26989b4d4c47fb8e029ad434aa4390367cd9c4
Peskovatskov Ignat <113009998+Peskovatskow-Ignat@users.noreply.github.com>
2023-03-03 14:04:22 +0300

    ThreadPool

--- SecondSem/ThreadPool/Task_2.py
+++ SecondSem/ThreadPool/Task_2.py
@@ -10,16 +10,18 @@ from concurrent.futures import ThreadPoolExecutor
 
 def numbers_1(queue, number):
     for i in range(1, number + 1):
-        num = random.randint(1, 1000000)
+        num = random.randint(1, 100)
         queue.put(num)
 
 
 def delenie(queue):
     while not queue.empty():
         num = queue.get()
+        list_1 = []
         for i in range(1, num + 1):
             if num % i == 0:
-                print(f"{i} - делитель числа {num}")
+                list_1.append(i)
+        print(f"{list_1} - делители числа {num}")
 
 
 def main():
